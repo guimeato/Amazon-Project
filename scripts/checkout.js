08d6af9e58dffac09e6917b32872337d7d3cb910
Guilherme Meato <guimeato@gmail.com>
2026-05-13 00:41:24 -0300

    feat:  deleting products from cart with delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import{cart} from '../data/cart.js';
+import{cart,removeFromCart} from '../data/cart.js';
 import{products} from '../data/products.js'
 import { formatCurrency } from './utils/money.js';
 
@@ -42,7 +42,7 @@ cart.forEach((cartItem)=>{
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-button" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -101,4 +101,12 @@ cart.forEach((cartItem)=>{
 
 document.querySelector('.js-order-summary').innerHTML = cartItemList;
 
+const deleteBtn = document.querySelectorAll('.js-delete-button');
 
+
+deleteBtn.forEach((deleteButton)=>{
+  deleteButton.addEventListener('click',()=>{
+    const productId = deleteButton.dataset.productId;
+    removeFromCart(productId);
+  });
+});
