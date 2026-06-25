3a86f8fde0221cfd6bc0fb1a5c60579478c9854e
Guilherme Meato <guimeato@gmail.com>
2026-06-24 22:23:11 -0300

    feat: make a GET request at practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import {Car, RaceCar} from '../data/car.js'
+import {Car, RaceCar} from '../data/car.js';
+import '../data/backend-practice.js';
 
 
 renderCheckoutHeader();
