f1d84c979ec0f4be95ef1fae90f6859b92909345
Guilherme Meato <guimeato@gmail.com>
2026-06-28 21:35:47 -0300

    feat: convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -92,3 +92,19 @@ export function updateQuantity(productId, newQuantity){
 
   saveToStorage();
 }
+
+export function loadCart(func){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', ()=>{
+    console.log(xhr.response);
+    
+
+    func();
+    });
+
+    
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,20 +2,59 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js'
 //import {Car, RaceCar} from '../data/car.js';
 //import '../data/backend-practice.js';
 
+
+
+new Promise((resolve)=>{
+  
+  loadProducts(()=>{
+    
+    resolve('value1');
+  });
+  
+}).then((value)=>{
+    console.log(value);
+
+    return new Promise((resolve)=>{
+      loadCart(()=>{
+        resolve();
+      });
+    });
+
+    
+  }).then(()=>{
+    renderCheckoutHeader();
+
+    renderOrderSummary();
+
+    renderPaymentSummary();
+  });
+
+
+
+
+
+
+
+/*
 loadProducts(()=>{
 
-  renderCheckoutHeader();
+  loadCart(()=>{
+    renderCheckoutHeader();
 
-  renderOrderSummary();
+    renderOrderSummary();
 
-  renderPaymentSummary();
+    renderPaymentSummary();
+  });
+
   
+
 });
 
-
+*/
 
 
 
