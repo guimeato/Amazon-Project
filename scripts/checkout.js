245a3f3c50dc6fc6e4be4c5f3efe8bfb82c69886
Guilherme Meato <guimeato@gmail.com>
2026-06-28 22:12:04 -0300

    feat: convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,41 +1,26 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js'
 //import {Car, RaceCar} from '../data/car.js';
 //import '../data/backend-practice.js';
 
 
-
-new Promise((resolve)=>{
-  
-  loadProducts(()=>{
-    
-    resolve('value1');
-  });
-  
-}).then((value)=>{
-    console.log(value);
-
-    return new Promise((resolve)=>{
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve)=>{
       loadCart(()=>{
         resolve();
       });
-    });
-
-    
-  }).then(()=>{
+    })
+]).then(()=>{
     renderCheckoutHeader();
 
     renderOrderSummary();
 
     renderPaymentSummary();
-  });
-
-
-
-
+});
 
 
 
